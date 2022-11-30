4ef5df695eecb68bd5a6c771e15c0b96ab905cb4
Rurik Pinheiro <rurikspin@gmail.com>
2022-11-30 12:41:05 -0300

    feat: create basket types

---
+++ src/basket/types.ts
@@ -0,0 +1,79 @@
+export interface BasketInput {
+  quantity: number;
+  description: string;
+  price: number;
+}
+
+export class BasketItem {
+  quantity: number;
+  description: string;
+  priceIncludingTax: number; // TODO: create a Price type
+  price: number;
+  tax: number;
+
+  constructor({ quantity, description, price }: BasketInput) {
+    this.quantity = quantity;
+    this.description = description;
+    this.priceIncludingTax = price * (1 + this.getTax());
+    this.tax = price * this.getTax();
+    this.price = price;
+  }
+
+  toString() {
+    return `${this.quantity} ${this.description} at ${this.price}`;
+  }
+
+  getTax() {
+    let total = 0;
+
+    if (this.description.includes("imported")) {
+      total = total + 5;
+    }
+
+    if (
+      !this.description.includes("chocolate") &&
+      !this.description.includes("pills") &&
+      !this.description.includes("book")
+    ) {
+      total = total + 10;
+    }
+
+    return total / 100;
+  }
+
+  getTotal() {
+    return this.quantity * this.priceIncludingTax;
+  }
+
+  getSalesTaxes() {
+    return this.quantity * this.tax;
+  }
+}
+
+export class ShoppingBasket {
+  items: BasketItem[];
+
+  constructor(items: BasketItem[]) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getSalesTaxes() {
+    let total = 0;
+    this.items.forEach((i) => {
+      total = total + i.getSalesTaxes();
+    });
+    return total;
+  }
+
+  getTotal() {
+    let total = 0;
+    this.items.forEach((i) => {
+      total = total + i.getTotal();
+    });
+    return total;
+  }
+}
